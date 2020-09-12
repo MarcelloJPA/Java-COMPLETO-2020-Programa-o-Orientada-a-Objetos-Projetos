d16a31b78f10ee74511b976394058733959d86ec
MarcelloJPA <cellolim4jpa@gmail.com>
2020-09-12 19:38:35 -0300

    12/09/2020

---
+++ ExercicioFor06/src/Ex06.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ex06 {
+
+	public static void main(String[] args) {
+		/*Ler um número inteiro N e calcular todos os seus divisores.
+ */
+
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			if(n % i == 0 ) {
+				System.out.println(i);
+			}
+		}
+		sc.close();
+		
+	}
+
+}
